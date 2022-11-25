a4ebe7bbe17199ec236bb65b4ac4151937e965ec
Cassin01 <koyojb@gmail.com>
2022-11-26 08:00:51 +0900

    fix: shebang

--- link.sh
+++ link.sh
@@ -13,3 +13,4 @@ ln -sf ~/.config/nixpkgs/cnf/xmobar/xmobarrc.hs ~/.config/xmobar/xmobarrc.hs
 ln -sf ~/.config/nixpkgs/cnf/xmonad/xmonad.hs ~/.config/xmonad/xmonad.hs
 ln -sf ~/.config/nixpkgs/cnf/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -sf ~/.config/nixpkgs/cnf/zsh/.zshrc.nixos ~/.config/zsh/.zshrc.nixos
+chmod u+x ~/.config/zsh/.zshrc.nixos
